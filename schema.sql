9f71cc8a97e9f80936b21683b838b7045e12e284
Ricky Mormor <rickymormor1@gmail.com>
2022-02-25 12:47:05 +0000

    Create owners and species table

--- schema.sql
+++ schema.sql
@@ -9,4 +9,19 @@ CREATE TABLE animals (
     weight_kg FLOAT NOT NULL
 );
 
-ALTER TABLE animals ADD species CHAR(20);
+ALTER TABLE animals
+ADD species CHAR(20);
+
+CREATE TABLE owners (
+    id SERIAL PRIMARY KEY,
+    full_name VARCHAR(20),
+    age INT
+);
+CREATE TABLE species (id SERIAL PRIMARY KEY, name VARCHAR(30));
+ALTER TABLE animals
+ADD PRIMARY KEY (id);
+ALTER TABLE animals DROP COLUMN species;
+ALTER TABLE animals
+ADD species_id INT REFERENCES species(id);
+ALTER TABLE animals
+ADD owner_id INT REFERENCES owners(id);
