a5259114c02f347ea6b2c75bc3e3336fb9f69dc4
Ricky Mormor <rickymormor1@gmail.com>
2022-03-01 08:58:04 +0000

    Add queries

--- queries.sql
+++ queries.sql
@@ -214,3 +214,7 @@ GROUP BY vet.name,
   sp.name
 ORDER BY COUNT DESC
 LIMIT 1;
+
+EXPLAIN ANALYSE SELECT COUNT(*) FROM visits where animal_id = 4;
+EXPLAIN ANALYSE SELECT * FROM visits where vet_id = 2;
+EXPLAIN ANALYSE SELECT * FROM owners where email = 'owner_18327@mail.com';
